91d9284c092a46d99dda7b6bd85e5921ca0b10e5
exerciceDclic <mamadicamara566@gmail.com>
2026-02-15 09:45:38 +0100

    cors

--- back/server.js
+++ back/server.js
@@ -16,7 +16,7 @@ connectDB().catch(err => {
 
 // CORS global
 app.use(cors({
-  origin: process.env.FRONTEND_URL ||'http://localhost:5173',
+  origin: ['https://mobile-unlock-store.vercel.app','http://localhost:5173'],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS']
 }));
