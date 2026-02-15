56239522d03746a0645e8ad2db79f9046bfe4f30
exerciceDclic <mamadicamara566@gmail.com>
2026-02-15 09:40:55 +0100

    modification de server.js et vercel.json

--- back/server.js
+++ back/server.js
@@ -16,7 +16,7 @@ connectDB().catch(err => {
 
 // CORS global
 app.use(cors({
-  origin: ['https://mobile-unlock-store.vercel.app','http://localhost:5173'],
+  origin: process.env.FRONTEND_URL ||'http://localhost:5173',
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS']
 }));
