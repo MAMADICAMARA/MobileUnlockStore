7e8972af1db686195db0f28ba30bd18e9147bc68
MAMADICAMARA <Mamadicamara566@gmail.com>
2026-06-25 17:54:06 +0000

    Update CORS origin to include new frontend URL

--- back/server.js
+++ back/server.js
@@ -16,11 +16,11 @@ connectDB().catch(err => {
 
 // CORS global
 app.use(cors({
-  origin: ['https://mobile-unlock-store.vercel.app','http://localhost:5173'],
+  origin: ['https://mobile-unlock-store.vercel.app',"https://mobileunlockstore-frontend.onrender.com"
+,'http://localhost:5173'],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS']
 }));
-
 
 // Parsers
 app.use(express.json({ limit: '10mb' }));
